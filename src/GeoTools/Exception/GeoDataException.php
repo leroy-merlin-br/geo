fddee52d4ab489e5f6a99b0267a9e192a4c9ecc2
Zizaco <zizaco@gmail.com>
2015-04-02 11:59:56 -0300

    Added GeoData especific exception

---
+++ src/GeoTools/Exception/GeoDataException.php
@@ -0,0 +1,12 @@
+<?php
+namespace LeroyMerlin\GeoTools\Exception;
+
+/**
+ * Means the provided geo data is invalid
+ *
+ * @package  LeroyMerlin\GeoTools
+ *
+ * @license  MIT
+ */
+class GeoDataException extends \Exception {
+}
